f2dbae45319e15b8a234a61eb8aa3fa35707a4f2
dhananjay <dhananjaykumarverma088@gmail.com>
2023-02-15 12:36:00 +0530

    mypoject

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+mongoose
+  .connect("mongodb+srv://data:data123@cluster0.dbeoyml.mongodb.net/?retryWrites=true&w=majority")
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
+
+app.listen(5000, () => {
+  console.log(" server is running!");
+});
+
